40aef9a4ad46653a59d9e5aa0c54b45b0b3a9beb
quangG01 <quydang1404@gmail.com>
2023-01-16 23:55:54 +0700

    HTML check

---
+++ main.js
@@ -0,0 +1,10 @@
+function showalert(yes,no)
+{
+    var chon;
+    var khong = document.getElementById(no);
+    var co = document.getElementById(yes);
+    if(chon == yes){
+        alert("Chọn yes đi em iu");
+    }
+    else return alert("em iu chọn no chưa,nếu chưa thì nhấn zô nha,còn níu rùi thì hjhj");
+}
